35eb70a690ff59c5651629123335442b57b922d2
Jonathan Sogg <JonathanSogg@fas.harvard.edu>
2014-10-23 01:51:16 -0700

    Fixed route method

--- app/routes.php
+++ app/routes.php
@@ -18,9 +18,9 @@ Route::get('/', function()
 
 Route::post('lorem', function()
 {
-	$number = Input::get('paragraphs');
+	$number = Input::post('paragraphs');
 	$generator = new Badcow\LoremIpsum\Generator();
-	$paragraphs = $generator->getParagraphs($number);
+	$paragraphs = $generator->getParagraphs($number => );
 	
 	return View::make('lorem', $paragraphs);
 });
