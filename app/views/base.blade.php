434dad6ca7a0e7b4124daf1eef57c16482caf27d
JonathanSogg <JonathanSogg@users.noreply.github.com>
2014-10-24 09:24:04 -0700

    Update base.blade.php

--- app/views/base.blade.php
+++ app/views/base.blade.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Project 3 - Laravel Basics</title>
+	{{ HTML::style(url('//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css')) }}
 </head>
 <body>
 	<div class="content">
