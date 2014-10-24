c744003daa95f3579d7ae0610e6d75c5fd8d886a
JonathanSogg <JonathanSogg@users.noreply.github.com>
2014-10-24 10:49:30 -0700

    changed persistence

--- app/views/users.blade.php
+++ app/views/users.blade.php
@@ -6,9 +6,9 @@
 		{{ Form::label('UserBox', 'Number of Users') }}
 		{{ Form::text('userno', $flags['userno']) }}
 		{{ Form::label('BdBox', 'Birthday?') }}
-		{{ Form::checkbox('bd', $flags['bd']) }}
+		{{ Form::checkbox('bd', 'bd', $flags['bd']) }}
 		{{ Form::label('BioBox', 'Bio?') }}
-		{{ Form::checkbox('bio', $flags['bio']) }}
+		{{ Form::checkbox('bio', 'bio', $flags['bio']) }}
 		{{ Form::submit('Generate!') }}
 	{{ Form::close() }}
 	<br />
