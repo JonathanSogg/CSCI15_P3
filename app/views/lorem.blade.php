7678a56cfcc28657246be4523d0854163262dd9f
Jonathan Sogg <JonathanSogg@fas.harvard.edu>
2014-10-23 23:52:05 -0700

    Git madness

--- app/views/lorem.blade.php
+++ app/views/lorem.blade.php
@@ -2,26 +2,13 @@
 	
 @section('content')
 	<p>This is my page, we're good for now. Forms go underneath.</p>
-	{{ Form::open(array('url' => 'users')) }}
-		{{ Form::label('Number of users', 'Users') }}
-		{{ Form::number('users', '1') }}
-		{{ Form::label('Address?', 'Address') }}
-		{{ Form::checkbox('address', 'address') }}
-		{{ Form::label('Bio?', 'Bio') }}
-		{{ Form::checkbox('bio', 'bio') }}
+	{{ Form::open(array('url' => 'lorem')) }}
+		{{ Form::label('paragraphs', 'Paragraphs') }}
+		{{ Form::text('paragraphs', '1') }}
 		{{ Form::submit('Generate!') }}
 	{{ Form::close() }}
 	<br />
-	@if (isset($user))
-		@for ($i=0; $i < users; $i++)
-			{{ $user->name }}
-			@if isset($addr)
-				{{ $user->address }}
-			@endif
-
-			@if isset($bio)
-				{{ $user->text }}
-			@endif
-		@endfor
+	@if (isset($paragraphs))
+		{{ implode('<p>', $paragraphs) }}
 	@endif
 @stop
