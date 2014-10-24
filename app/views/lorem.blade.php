7111b296706958463f5dade801a4c54b6e6cdb91
JonathanSogg <JonathanSogg@users.noreply.github.com>
2014-10-24 10:57:10 -0700

    changed variable name

--- app/views/lorem.blade.php
+++ app/views/lorem.blade.php
@@ -4,7 +4,7 @@
 	<p>This is the Lorem Ipsum generator. Specify the number of paragraphs you want and click "Generate"</p>
 	{{ Form::open(array('url' => 'lorem')) }}
 		{{ Form::label('paragraphs', 'Paragraphs') }}
-		{{ Form::text('paragraphs', 'paragraphs', $paras) }}
+		{{ Form::text('paragraphs', 'paragraphs', $number) }}
 		{{ Form::submit('Generate!') }}
 	{{ Form::close() }}
 	<br />
