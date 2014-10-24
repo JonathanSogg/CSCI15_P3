22aa6066f650db351f8883a08690ccd2148217e9
JonathanSogg <JonathanSogg@users.noreply.github.com>
2014-10-24 09:50:41 -0700

    Added in page title

--- app/views/base.blade.php
+++ app/views/base.blade.php
@@ -3,9 +3,10 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Project 3 - Laravel Basics</title>
-	{{ HTML::style(url('//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css')) }}
+	{{ HTML::style(url('//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/darkly/bootstrap.min.css')) }}
 </head>
 <body>
+	<h1>Lorem Ipsum and User Generator</h1>
 	<div class="content">
 	@yield('content')
 	</div>
